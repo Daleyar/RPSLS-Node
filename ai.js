a14f982e42073bc6d1d540c660c5d72de6e21cf1
Dom B <domlamont@yahoo.com>
2021-11-09 09:43:28 -0800

    Updated ai function

---
+++ ai.js
@@ -0,0 +1,14 @@
+const player = require("./player");
+
+class AI extends player{
+    constructor(){
+        super(this.name, this.choice);
+    }
+
+    selectGesture(){
+        let randomNum = Math.floor(Math.random() * );
+    }
+
+}
+
+module.extends.ai = ai;
