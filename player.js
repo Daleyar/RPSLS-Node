635e4e47f387c73a61daa54bd8e7548dfd9e9424
Ali Daley <ali.daley72@gmail.com>
2021-11-09 12:27:45 -0500

    initialized the player class

---
+++ player.js
@@ -0,0 +1,18 @@
+class player {
+    constructor(){
+        this.name = " ";
+        this.score = 0;
+        this.choice = " ";
+        this.gestureList = ["rock","paper","scissors","lizard","spock"]
+    }
+
+    chooseGesture(){
+        break;
+    }
+
+    defineName(){
+        break;
+    }
+
+}
+
